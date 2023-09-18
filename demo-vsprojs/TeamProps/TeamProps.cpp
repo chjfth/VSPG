9477eae093c202e5a853c7f99d060085265e56f5
Jimm Chen WDK10 <chjfth@gmail.com>
2023-09-18 13:56:01 +0800

    Update TeamProps demo according to v603.

--- demo-vsprojs/TeamProps/TeamProps.cpp
+++ demo-vsprojs/TeamProps/TeamProps.cpp
@@ -8,35 +8,37 @@ int _tmain(int argc, TCHAR* argv[])
 	setlocale(LC_ALL, "");
 	
 	_tprintf(_T("Hello, TeamProps!\n"));
-	_tprintf(_T("sizeof(TCHAR)=%d\n"), sizeof(TCHAR));
+	_tprintf(_T("sizeof(TCHAR)=%d\n"), (int)sizeof(TCHAR));
 	return 0;
 }
 
 /*
- * With VSPG.Team.props and Team-Postbuild.bat Build,
- * Project build output is like follows.
- * We see that env-var TeamName,ConfigurationType and ProjectGuid is made available for VSGP bat to use.
+
+With the customized VSPU.props, Project build output is like follows.
+We see that env-var TeamName, ConfigurationType and ProjectGuid is made available for VSPG bat to use.
 
 1>------ Build started: Project: TeamProps, Configuration: Debug Win32 ------
-1>Build started 2022-10-13 9:39:49 PM.
+1>Build started 2023-09-18 13:48:29.
+1>VSPG_ShowLoaded:
+1>  VSPG version 603 loaded from: D:\gitw\VSPG\_VSPG\boots
 1>InitializeBuildStatus:
-1>  Creating "D:\gitw\VSPG\vsprojs\TeamProps\obj-v100\Win32\Debug\TeamProps.unsuccessfulbuild" because "AlwaysCreate" was specified.
+1>  Creating "D:\gitw\VSPG\demo-vsprojs\TeamProps\obj-v100\Win32\Debug\TeamProps.unsuccessfulbuild" because "AlwaysCreate" was specified.
 1>ClCompile:
-1>  All outputs are up-to-date.
+1>  TeamProps.cpp
 1>Link:
-1>  All outputs are up-to-date.
-1>  TeamProps.vcxproj -> D:\gitw\VSPG\vsprojs\TeamProps\bin-v100\Win32\Debug\TeamProps.exe
+1>  TeamProps.vcxproj -> D:\gitw\VSPG\demo-vsprojs\TeamProps\bin-v100\Win32\Debug\TeamProps.exe
 1>VSPG_PostBuild:
-1>  ...[Team-Postbuild.bat] START from "D:\gitw\VSPG\vsprojs\TeamProps"
-1>  ...[Team-Postbuild.bat] TeamName = Wanderer
-1>  ...[Team-Postbuild.bat] ConfigurationType = Application
-1>  ...[Team-Postbuild.bat] ProjectGuid = {20221012-0000-0000-0000-132030000002}
+1>  ...[VSPU-PostBuild.bat] START from "D:\gitw\VSPG\demo-vsprojs\TeamProps\_VSPG"
+1>  ...[VSPU-PostBuild.bat] TeamName = Wanderer
+1>  ...[VSPU-PostBuild.bat] ConfigurationType = Application
+1>  ...[VSPU-PostBuild.bat] ProjectGuid = {20221012-0000-0000-0000-132030000002}
 1>FinalizeBuildStatus:
-1>  Deleting file "D:\gitw\VSPG\vsprojs\TeamProps\obj-v100\Win32\Debug\TeamProps.unsuccessfulbuild".
-1>  Touching "D:\gitw\VSPG\vsprojs\TeamProps\obj-v100\Win32\Debug\TeamProps.lastbuildstate".
+1>  Deleting file "D:\gitw\VSPG\demo-vsprojs\TeamProps\obj-v100\Win32\Debug\TeamProps.unsuccessfulbuild".
+1>  Touching "D:\gitw\VSPG\demo-vsprojs\TeamProps\obj-v100\Win32\Debug\TeamProps.lastbuildstate".
 1>
 1>Build succeeded.
 1>
-1>Time Elapsed 00:00:01.20
+1>Time Elapsed 00:00:02.48
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
 
 */
